160f239aa445de4b558c7542fcd7181252c1586e
COGNIO-SO <asset@thealgohype.com>
2026-05-14 19:11:48 +0530

    feat: add global cyber-themed styles and Tailwind configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app';
+import { Share_Tech_Mono } from 'next/font/google';
 
 import '../styles/globals.css';
 
+const shareTechMono = Share_Tech_Mono({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={shareTechMono.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
